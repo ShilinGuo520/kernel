64d5d7c598a0c175fb206cb33d63a4c5c79cfa53
shilin.guo <552002499@qq.com>
2016-09-02 21:27:39 +0800

    Add key config
     Changes to be committed:
    	modified:   arch/arm/mach-s3c24xx/mach-mini2440.c

--- linux-4.6.3/arch/arm/mach-s3c24xx/mach-mini2440.c
+++ linux-4.6.3/arch/arm/mach-s3c24xx/mach-mini2440.c
@@ -335,45 +335,29 @@ static struct platform_device mini2440_device_eth = {
  */
 static struct gpio_keys_button mini2440_buttons[] = {
 	{
-		.gpio		= S3C2410_GPG(0),		/* K1 */
+		.gpio		= S3C2410_GPF(0),		/* K1 */
 		.code		= KEY_F1,
 		.desc		= "Button 1",
 		.active_low	= 1,
 	},
 	{
-		.gpio		= S3C2410_GPG(3),		/* K2 */
+		.gpio		= S3C2410_GPF(2),		/* K2 */
 		.code		= KEY_F2,
 		.desc		= "Button 2",
 		.active_low	= 1,
 	},
 	{
-		.gpio		= S3C2410_GPG(5),		/* K3 */
+		.gpio		= S3C2410_GPG(3),		/* K3 */
 		.code		= KEY_F3,
 		.desc		= "Button 3",
 		.active_low	= 1,
 	},
 	{
-		.gpio		= S3C2410_GPG(6),		/* K4 */
-		.code		= KEY_POWER,
-		.desc		= "Power",
+		.gpio		= S3C2410_GPG(11),		/* K4 */
+		.code		= KEY_F4,
+		.desc		= "Button 4",
 		.active_low	= 1,
 	},
-	{
-		.gpio		= S3C2410_GPG(7),		/* K5 */
-		.code		= KEY_F5,
-		.desc		= "Button 5",
-		.active_low	= 1,
-	},
-#if 0
-	/* this pin is also known as TCLK1 and seems to already
-	 * marked as "in use" somehow in the kernel -- possibly wrongly */
-	{
-		.gpio		= S3C2410_GPG(11),	/* K6 */
-		.code		= KEY_F6,
-		.desc		= "Button 6",
-		.active_low	= 1,
-	},
-#endif
 };
 
 static struct gpio_keys_platform_data mini2440_button_data = {
